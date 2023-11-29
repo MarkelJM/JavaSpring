0bef11d978bd170c10ab73cc28b5b54261f94c7c
MarkelJM <markjuaristi@gmail.com>
2023-11-29 10:14:36 +0100

    updated methods

--- app1/src/main/java/com/example/app1/controllers/ClientController.java
+++ app1/src/main/java/com/example/app1/controllers/ClientController.java
@@ -3,31 +3,78 @@ package com.example.app1.controllers;
 import com.example.app1.entities.Client;
 import com.example.app1.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "clients") //al controlador se le añade un path
+@RequestMapping(path = "client") //al controlador se le añade un path
 public class ClientController {
-    @Autowired private ClientRepository repoCliente;
+    @Autowired
+    private ClientRepository repoCliente;
 
-    @RequestMapping(path = "/")  //....../client/
-    public List<Client> allClient(){
+    //@RequestMapping(path = "/", method = RequestMethod.GET)  //....../client/  @GetMapping(path = "/")
+    @GetMapping(path = "/") //--> esto o esto: @RequestMapping(path = "/", method = RequestMethod.GET)
+    public List<Client> allClient() {
         System.out.println("repoClient");
         return repoCliente.findAll();
 
     }
 
-    @RequestMapping(path = "{id:\\d{0,5}}") // \\ == \ y la d -> indica un número
-    public Client selectClient(@PathVariable("id") int codigo){ //si el path y el parametro son distintos en PathVariable("id")
+    @GetMapping(path = "/{id:\\d{0,5}}") // \\ == \ y la d -> indica un número
+    public Client selectClient(@PathVariable("id") int codigo) { //si el path y el parametro son distintos en PathVariable("id")
         //return new Client("Markel"); //actualizado a find byID
-        return repoCliente.findById(codigo).orElse(null);
+        return repoCliente.findById(codigo).get();
 
     }
+
+    /*
+    @PostMapping(path = "/") //POST: client
+    //No funciona, solucion abajo
+    public void insertClient(Client client){
+        repoCliente.save(client);
+    }
+    */
+    @PostMapping(path = "/") //POST: client
+    public ResponseEntity<Client> inserClient(@RequestBody Client client) {
+        client.setId(-1);//si el usuario  indica en el json un id, aqui indicamos que el id se cambia a-1 y como no acepta menor de 0 crea un nuevo seguido al ultimo
+        var c = repoCliente.save(client); //@RequestBody busca objetos json o algo asi
+        return ResponseEntity.ok(c);
+
+    }
+
+    // actualizar todo con un PUT
+    @PutMapping(path = "/") //POST: client
+    public ResponseEntity<Void> updateClient(@RequestBody Client client) {
+        if (!repoCliente.existsById(client.getId())) {
+            return ResponseEntity.notFound().build();
+            //throw new RuntimeException("No existe el registro");
+        }
+        repoCliente.save(client); //@RequestBody busca objetos json o algo asi
+        return ResponseEntity.ok(null);
+
+
+    }
+
+    @PatchMapping (path = "/") //POST: client
+    public ResponseEntity<Void> updateClient(@RequestBody Client client) {
+        if (!repoCliente.existsById(client.getId())) {
+            return ResponseEntity.notFound().build();
+            //throw new RuntimeException("No existe el registro");
+        }
+        repoCliente.save(client); //@RequestBody busca objetos json o algo asi
+        return ResponseEntity.ok(null);
+
+
+    }
+
+
+
+
+
+
 
 
 
