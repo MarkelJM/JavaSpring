0bdefba66c7722be00cd7eac6bac1a7c67bd05d2
MarkelJM <markjuaristi@gmail.com>
2023-11-29 13:58:12 +0100

    implemented html and Model for Page Controller

--- app1/src/main/java/com/example/app1/controllers/PagesControllers.java
+++ app1/src/main/java/com/example/app1/controllers/PagesControllers.java
@@ -1,13 +1,33 @@
 package com.example.app1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/page")
+@RequestMapping("page")
 public class PagesControllers {
     //metodo para solicitar una pagina usando Thymeleaf
-    public String pageHome(){
+
+
+    @GetMapping
+    public String pageHome(Model model){
+        model.addAttribute("nombre", "Markel");
         return  "home";
     }
+
+
+    /*
+    //2.opcion
+
+    @GetMapping
+    public String pageHome2(){
+        var model = new ModelAndView();
+        model.addObject("nombre", "Markel";
+        return  "home";
+    }
+    */
+
 }
