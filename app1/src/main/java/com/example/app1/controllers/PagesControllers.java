84eea4f197b13217b3c97bef090bc9734ed5c2b1
MarkelJM <markjuaristi@gmail.com>
2023-11-30 10:47:58 +0100

    created CompraForm model and edited pagescontroller to redirections of compra page

--- app1/src/main/java/com/example/app1/controllers/PagesControllers.java
+++ app1/src/main/java/com/example/app1/controllers/PagesControllers.java
@@ -1,8 +1,12 @@
 package com.example.app1.controllers;
 
+import com.example.app1.models.CompraForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +27,28 @@ public class PagesControllers {
 
         return  "home";
     }
+
+    @GetMapping("compra")
+    public String purchaseItem(Model model){
+        model.addAttribute("compra", new CompraForm(10.0, ""));
+        return  "compra";
+    }
+
+    @PostMapping("compra")
+    public String purchaseItem(Model model, @ModelAttribute CompraForm compra, Errors error){
+        if(compra.getPrecio() < 1.0){
+            error.rejectValue("precio", "", "");
+        }
+
+        if(error.hasErrors()){
+            model.addAttribute("compra",compra);
+            return  "compra";
+        }
+        System.out.println(compra);
+        return "redirect:compra";
+    }
+
+
 
 
     /*
