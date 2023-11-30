320bcda8b237ca9b5e5c37e82379746041cc8863
MarkelJM <markjuaristi@gmail.com>
2023-11-30 12:43:42 +0100

    personalized messaged and properties done

--- app1/src/main/java/com/example/app1/App1Application.java
+++ app1/src/main/java/com/example/app1/App1Application.java
@@ -10,20 +10,25 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
 
 @SpringBootApplication
+@PropertySource("classpath:messages.properties")
 public class App1Application {
 	/*
 	@Autowired
 	@Qualifier( "name")
 	private  String name;
 	*/
-	@Value("${username}")
-	private  String username;
+
 
 
 	public static void main(String[] args) {
 		var context = SpringApplication.run(App1Application.class, args);
+
 		//final var c1 = new Client();
 		//c1.getId();
 
@@ -41,6 +46,8 @@ public class App1Application {
 
 	}
 
+
+
 	//EJEMPLO BEANS
 	/* los @Bean y @Autowired se conectan con nombre y/o tipo de dato(si solo hay uno de ese tipo de dato)*/
 	@Bean("nombre")
@@ -49,10 +56,20 @@ public class App1Application {
 		return "Markel";
 	}
 
+
+	@Value("${user.name}")
+	private  String username;
+
+	@Value("${hello: valor por deferctor}")
+	private String hello;
+
 	@PostConstruct
 	public void postConstructor(){
-		System.out.println(">>>>" +username);
+		System.out.println(">>>>    " +username);
+		System.out.println(">>>>>   " + hello);
+		System.getProperties().list(System.out);
 	}
+
 
 
 
